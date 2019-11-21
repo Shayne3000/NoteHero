2062e4bece6902db204f11349172f73cb69e8c5f
Seni Joshua <alfred3000prime@hotmail.com>
2019-11-21 00:58:55 -0500

    Combined Room Entities and Retrofit data class using the @Embedded annotation

--- app/src/main/java/com/senijoshua/notehero/data/sources/local/entity/Thumb.kt
+++ app/src/main/java/com/senijoshua/notehero/data/sources/local/entity/Thumb.kt
@@ -1,5 +1,7 @@
 package com.senijoshua.notehero.data.sources.local.entity
 
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.squareup.moshi.JsonClass
@@ -8,5 +10,19 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class Thumb(
     @PrimaryKey val id: Int,
-    val description: String
+    val description: String = "",
+    @Embedded val user: User,
+    @Embedded val urls: Urls
 )
+
+data class User(@ColumnInfo(name = "thumb_creator_id") val id: String = "",
+                @ColumnInfo(name = "thumb_name") val name: String = "",
+                val portfolio_url: String = "",
+                val instagram_username: String = ""
+                )
+
+data class Urls(@ColumnInfo(name = "full_image") val full: String = "",
+                @ColumnInfo(name = "regular_image") val regular: String = "",
+                @ColumnInfo(name = "small_image") val small: String = "",
+                @ColumnInfo(name = "thumb_image") val thumb: String = ""
+               )
--- app/src/main/java/com/senijoshua/notehero/data/sources/remote/ThumbRemote.kt
+++ app/src/main/java/com/senijoshua/notehero/data/sources/remote/ThumbRemote.kt
@@ -1,5 +1,6 @@
 package com.senijoshua.notehero.data.sources.remote
 
+import com.senijoshua.notehero.data.sources.local.entity.Thumb
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -15,5 +16,5 @@ interface ThumbRemote {
     suspend fun getThumbnails(@Query("page") pageNumber: Int = 1,
                       @Query("per_page") itemsPerPage: Int = 20,
                               @Query("order_by") order: String = "popular"
-                             ): Response<List<ThumbRemote>>
+                             ): Response<List<Thumb>>
 }
