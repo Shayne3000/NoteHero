30866ddc02dcc3cf2276230ecb3337e7c378e319
Seni Joshua <alfred3000prime@hotmail.com>
2020-01-06 23:38:10 -0500

    Added destinations to the nav graph and performed navigation using the nav controller

---
+++ app/src/main/java/com/senijoshua/notehero/presentation/details/NoteDetailsFragment.kt
@@ -0,0 +1,12 @@
+package com.senijoshua.notehero.presentation.details
+
+import android.os.Bundle
+import com.senijoshua.notehero.R
+import com.senijoshua.notehero.presentation.base.NoteHeroFragment
+
+class NoteDetailsFragment : NoteHeroFragment(R.layout.fragment_note_details) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
